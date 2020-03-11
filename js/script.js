0e33b65830cb289630a40f65863498b37db9d987
matm27000 <mateusmartins27000@gmail.com>
2020-03-11 05:48:40 -0300

    correção para o logo não sair da tela na movimentação

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 var id = setInterval(movimenta,10);
 const altWind = window.innerHeight;
+const logoHeight = document.getElementById('logo').clientHeight
 var y = 0;
 function movimenta(){
     var logo = document.getElementById('logo')
-    if(y==altWind){
+    if(y==(altWind - logoHeight)){
         clearInterval(id);
     } else{
         y++;
