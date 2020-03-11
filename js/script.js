589cbb119e7c2753e1976746b8494e509c7178c0
matm27000 <mateusmartins27000@gmail.com>
2020-03-11 07:24:35 -0300

    criada movimentação diagonal com colisão

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
-var id = setInterval(movimenta,10);
+var id = setInterval(movimenta,1);
 const altWind = window.innerHeight;
+const largWind = window.innerWidth;
 const logoHeight = document.getElementById('logo').clientHeight
+const logoWidth = document.getElementById('logo').clientWidth
+const maxHeight = altWind - logoHeight
+const maxWidth = largWind - logoWidth
 var y = 0;
+var x = 0;
+var xAd = 1; 
+var yAd = 1; 
 function movimenta(){
     var logo = document.getElementById('logo')
-    if(y==(altWind - logoHeight)){
-        clearInterval(id);
-    } else{
-        y++;
-        logo.style.top = y + "px";
+    y+=yAd;
+    x+=xAd;
+    logo.style.top = y + "px";
+    logo.style.left = x + "px";
+    if(y>=maxHeight || y<=0){
+        yAd *= -1;
+    } 
+    if(x>=maxWidth || x<=0){
+        xAd *= -1;
     }
-    
-
 }
