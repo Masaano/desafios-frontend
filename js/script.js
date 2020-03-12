e5eab814a1dff50d008b2ae86986aa962a456624
matm27000 <mateusmartins27000@gmail.com>
2020-03-12 02:09:57 -0300

    criada a fuction de randomização das cores

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var id = setInterval(movimenta,1);
+
 const altWind = window.innerHeight;
 const largWind = window.innerWidth;
 const logoHeight = document.getElementById('logo').clientHeight
@@ -9,6 +10,21 @@ var y = 0;
 var x = 0;
 var xAd = 1; 
 var yAd = 1; 
+
+
+function randomColorApi() {
+    return fetch("/api/cores.json")
+      .then(function (response){
+          return response.json()
+      })
+      .then(function(cor) {
+        var posit = Math.floor(Math.random()*20);
+        return cor.colors[posit];
+      })
+
+      .catch((error) => console.log("error"));
+  }
+
 function movimenta(){
     var logo = document.getElementById('logo')
     y+=yAd;
